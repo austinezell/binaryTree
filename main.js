d976a2e71eabdfdf64b35a556d0b69a7d58ef123
austin ezell <austin.r.ezell@gmail.com>
2016-03-08 10:46:55 -0800

    tidies

--- main.js
+++ main.js
@@ -1,47 +1,39 @@
 "use strict";
 const util = require('util')
 
-let nums = [];
-
-for (let i = 0; i<50; i++){
-  let randomNum = Math.floor(Math.random()*100);
-  if(nums.indexOf(randomNum) < 0) {
-    nums.push(randomNum);
-  }
-}
-
 class Branch {
   constructor(value){
     this.value = value;
     this.left = null;
     this.right = null;
   }
-  nextDirection(value){
-    return value > this.value ? "right" : "left"
-  }
 }
 
 class BinaryTree {
   constructor(values){
-    values = Array.from(values);
     values.sort((a,b)=>a-b);
-    let rootValue = values[Math.floor(values.length/2)];
     // creates top node
+    let rootValue = values[Math.floor(values.length/2)];
     this._root = new Branch(rootValue);
+
     //generates halfway points for all other values to fall in between
     this.generateMidPoints("right", values);
     this.generateMidPoints("left", values);
     // adds remaining values
     for (let value of values){
-      this.addValue(value);
+      this.addValue(value)
     }
+    console.log(util.inspect(this._root, {showHidden: false, depth: null}));
+  }
+
+  display(){
   }
 
   addValue(value){
     if (!this.contains(value)){
       let currentBranch = this._root;
       while (currentBranch.value !== value){
-        let direction = currentBranch.nextDirection(value);
+        let direction = value < currentBranch.value ? "left" : "right";
         if (currentBranch[direction] === null){
           //generates new branch in direction
           currentBranch[direction] = new Branch(value);
@@ -49,11 +41,10 @@ class BinaryTree {
         currentBranch = currentBranch[direction];
       }
     }
-    // console.log(util.inspect(this._root, {showHidden: false, depth: null}));
   }
 
   toArray(){
-    function getValue(branch){
+    return (function getValue(branch){
       let values = [];
       values.push(branch.value);
       if (branch.left && branch.right) {
@@ -62,8 +53,7 @@ class BinaryTree {
       if (branch.right) return values.concat(getValue(branch.right));
       if (branch.left) return values.concat(getValue(branch.left));
       return values;
-    }
-    return getValue(this._root).sort((a,b)=>a-b);
+    })(this._root)
   }
 
   sum(){
@@ -90,6 +80,16 @@ class BinaryTree {
       currentBranch = value < currentBranch.value ? currentBranch.left : currentBranch.right
     }
     return false
+  }
+}
+
+
+let nums = [];
+
+for (let i = 0; i<50; i++){
+  let randomNum = Math.floor(Math.random()*100);
+  if(nums.indexOf(randomNum) < 0) {
+    nums.push(randomNum);
   }
 }
 
